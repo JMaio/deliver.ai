030a3f7ae2a68669264dfd95d996427441cee17c
JMaio <j.f.maio98@gmail.com>
2019-02-16 23:08:50 +0000

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from recipient import Recipient
 
 app = Flask(__name__)
+curr_username = "ash.ketchum"
+recipients = Recipient.from_file("recipients.txt")
+recv_map = {recv.username: recv for recv in recipients}
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        title="Secure Office Delivery",
+        username=curr_username,
+    )
+
+
+@app.route('/deliver/')
+def recipients_index():
+    return render_template(
+        'recipients.html',
+        username=curr_username,
+        recipients=recipients,
+    )
+
+
+@app.route('/deliver/<string:username>')
+def recipient_info(username):
+    recipient = recv_map[username]
+    return render_template(
+        'recipient_info.html',
+        username=curr_username,
+        recipient=recipient,
+    )
+
+
+@app.route('/receive/')
+def receive():
+    return render_template(
+        'base.html',
+        # username=username,
+        # offices=offices,
+    )
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # TODO authenticate the user
+        pass
+    return "this is a login screen"
+
+
+@app.route('/send-delivery', methods=['POST'])
+def send_delivery():
+    pass
 
 
 if __name__ == '__main__':
