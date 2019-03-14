519f8215ec9d85cbcc1bceabbe035349177401d6
Joao Maio <j.f.maio98@gmail.com>
2019-03-14 18:49:08 +0000

    fix absolute path error

--- flask_app/install_web_app.py
+++ flask_app/install_web_app.py
@@ -7,10 +7,12 @@ import os
 
 def main():
     PRE = '\n[deliver.ai] --> '
+    APP_PATH = os.path.dirname(os.path.abspath(__file__))
+    VENV_PATH = os.path.join(APP_PATH, 'v   env')
 
     print(" --- ##   deliver.ai web app intaller   ## --- ")
 
-    # # create the virtual enviroment
+    # create the virtual enviroment
     print(PRE + "Creating the virtual enviroment...")
 
     class ExtendedEnvBuilder(venv.EnvBuilder):
@@ -19,25 +21,24 @@ def main():
             super().post_setup(context)
 
     v = ExtendedEnvBuilder(with_pip=True)
-    v.create('venv')
+    v.create(VENV_PATH)
 
     path = os.getenv('VIRTUAL_ENV', None)
     if not path:
         raise Exception("Could not create virtual environment!")
 
-    print(PRE + "Virtual environment created at: {}".format(path))
+    print(PRE + "Virtual environment created at: {}".format(APP_PATH))
 
     py_path = os.path.join(path, 'python')
 
     # upgrade pip
     print(PRE + "Upgrading pip:")
-    upgrade_pip = subprocess.run(
-        [py_path, '-m', 'pip', 'install', '--upgrade', 'pip'])
+    subprocess.run([py_path, '-m', 'pip', 'install', '--upgrade', 'pip'])
 
-    # install using pip (https://stackoverflow.com/questions/12332975/installing-python-module-within-code)
+    # install using pip (https://stackoverflow.com/a/15950647/9184658)
     req = 'requirements.txt'
     print(PRE + "Installing dependencies from file: {}".format(req))
-    install_req = subprocess.run([py_path, '-m', 'pip', 'install', '-r', req])
+    subprocess.run([py_path, '-m', 'pip', 'install', '-r', req])
 
     print(PRE + "Install successful!")
 
