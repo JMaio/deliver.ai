a86e552cfe164f9f3f36ac23ca40683b1f3234a7
JMaio <10330349+JMaio@users.noreply.github.com>
2019-03-28 21:29:12 +0000

    improve ev3 testing script

--- flask_app/deliverai_testing/ev3_requests.py
+++ flask_app/deliverai_testing/ev3_requests.py
@@ -2,16 +2,22 @@ import requests
 import random
 import time
 
+
+def send_update(name='lion', battery=random.random() * 2.5 + 5,
+                x=random.randint(-5, 5), y=random.randint(-5, 5)):
+    r = requests.post(
+        'http://localhost:5000/api/botinfo',
+        params={
+            'name': name,
+            'battery_volts': battery,
+            'x_loc': x,
+            'y_loc': y,
+        }
+    )
+    return r.text
+
+
 if __name__ == "__main__":
     while True:
-        r = requests.post(
-            'http://localhost:5000/api/botinfo',
-            params={
-                'name': 'lion',
-                'battery_volts': random.random() * 2.5 + 5,
-                'x_loc': random.randint(-5, 5),
-                'y_loc': -random.randint(-5, 5)
-            }
-        )
-        print(r.text)
+        print(send_update())
         time.sleep(5)
